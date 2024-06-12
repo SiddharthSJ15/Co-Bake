8f2d942c618fd1c4c7c7a7e3e4a9356c387a80ab
SiddharthSJ15 <siddharthsj1011@gmail.com>
2024-06-12 14:49:42 +0530

    Search Bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Home.css";
 import homeImage from "../assets/home.jpeg"
+import "./Search.css";
 function Home() {
     return (
       <div className="container mt-5 pt-5">
         <div className="row mt-auto ">
           <div className="col text-white text-center ">
-            <p style={{ fontFamily: 'Montserrat, sans-serif', fontWeight: "300", fontSize: "56.51px"}}>
-              Start Typing...
-            </p>
-            <p style={{ fontFamily: 'Poppins, sans-serif', fontWeight: "300", fontSize: "14.97px"}}>
+
+            <input placeholder="Start Typing...." class="input" name="text" type="text" />
+
+            <p style={{ fontFamily: 'Poppins, sans-serif', fontWeight: "300", fontSize: "14.97px",color:"lightgray"}}>
               Press enter to begin your search
             </p>
           </div>
